28ffb2bee68a6d65c1028e0dbe9bd292e2479496
Dr. Atul Tiwari <atultiwari.in@gmail.com>
2024-09-10 15:59:06 +0530

    recreated model with updated libraries

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from Orange.data import Domain, DiscreteVariable, ContinuousVariable
 import numpy as np
 import pandas as pd
 
-with open("diabetes_rf_model.pkcls", "rb") as model:
+with open("diabetes_2024", "rb") as model:
     loaded_model = pickle.load(model)
 
 header = st.container()
